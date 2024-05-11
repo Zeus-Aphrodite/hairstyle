70943ab46f7a1719beb7899d895deb91b3612650
Zeus-Aphrodite <zeusa1398@gmail.com>
2024-05-11 14:37:24 +0900

    hair_fir

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,5 @@
 <?php
-
+  
 use Illuminate\Foundation\Inspiring;
 
 /*
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/    
 
 Route::get('/', 'HomepageController@index');
 Route::get('clear/cache/{custom_command?}', 'HomepageController@index');
